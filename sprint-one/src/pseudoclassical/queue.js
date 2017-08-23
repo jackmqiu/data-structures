8e09328a9fa2939c308049f1c2b06e541f02bf92
Pegasus <pegasus@yunio.com>
2017-08-23 15:37:23 -0700

    Revert "all done"
    This reverts commit db94ae7c5f00ca7627ba2424c4ccb145d7d258ec.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,26 +1,6 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  this.count = 0
-  this.storage = {}
 };
 
-Queue.prototype.enqueue = function(value) {
-	for(let index in this.storage) {
-      this.storage[this.count - index] = this.storage[this.count - index - 1]
-    }
-    this.storage[0] = value
-    this.count++
-}
 
-Queue.prototype.dequeue = function() {
-	if(this.count > 0) { 
-      this.count--
-      let result = this.storage[this.count]
-      return result
-    }
-}
-
-Queue.prototype.size = function() {
-	return this.count
-}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,25 +1,6 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  this.count = 0
-  this.stack = {}
-
 };
 
-Stack.prototype.push = function(value) {
-	this[this.count] = value
-	this.count ++
-}
 
-Stack.prototype.pop = function() {
-	let result
-	if(this.count > 0) {
-		this.count --
-		result = this[this.count]
-	}
-	return result
-}
-
-Stack.prototype.size = function() {
-	return this.count
-}
