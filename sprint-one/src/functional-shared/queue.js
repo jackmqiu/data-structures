fc30aaaba8bb793b30d982e4d701850bbe10bb19
Hack Reactor Student <student@hackreactor.com>
2017-08-23 15:22:54 -0700

    functional shared queue works

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var queue ={};
+  queue.storage = {}
+  queue.count = 0;
+  extend(queue, queueMethods);
+  return queue;
 };
+
+var extend = function(to, from){
+  for(key in from){
+    to[key] = from[key];
+  }
+}
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  for (index in this.storage){
+    this.storage[this.count - index] = this.storage[this.count - index - 1];
+  }
+  this.count++;
+  this.storage[0] = value;
+}
 
+queueMethods.dequeue = function(){
+  if(this.count === 0){
+    return;
+  }
+  this.count--;
+  return this.storage[this.count];
+}
+
+queueMethods.size = function(){
+  return this.count;
+}
