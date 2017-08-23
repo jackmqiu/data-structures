b6fd5ca1221f51f6b7f7611148da9edda80ccb0e
Hack Reactor Student <student@hackreactor.com>
2017-08-23 15:33:19 -0700

    all parts of part 1 passed

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.count = 0;
+  queue.storage = {};
+  return queue;
 };
+
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  for (index in this.storage){
+    this.storage[this.count - index] = this.storage[this.count - index - 1];
+  }
+  this.count++;
+  this.storage[0] = value;
+}
 
+queueMethods.dequeue = function(){
+  if(this.count === 0){
+    return;
+  }
+  this.count--;
+  return this.storage[this.count];
+}
+
+queueMethods.size = function(){
+  return this.count;
+}
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
 
+Queue.prototype.enqueue = function(value){
+  for (index in this.storage){
+    this.storage[this.count - index] = this.storage[this.count - index - 1];
+  }
+  this.count++;
+  this.storage[0] = value;
+}
+
+Queue.prototype.dequeue = function(){
+  if(this.count === 0){
+    return;
+  }
+  this.count--;
+  return this.storage[this.count];
+}
+
+Queue.prototype.size = function(){
+  return this.count;
+}
