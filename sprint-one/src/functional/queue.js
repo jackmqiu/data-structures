87b3f74d9828fabeb2b9fe3ca2a773c8f46e2caf
Hack Reactor Student <student@hackreactor.com>
2017-08-23 15:03:59 -0700

    queue works

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    for (index in storage){
+      storage[count - index] = storage[count - index - 1];
+    }
+    count++;
+    storage[0] = value;
   };
 
   someInstance.dequeue = function() {
+    if(count === 0){
+      return;
+    }
+    count--;
+    return storage[count];
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
