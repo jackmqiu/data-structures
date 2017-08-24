198487f0f51735789ed7df466ae0491b6369ceea
Hack Reactor Student <student@hackreactor.com>
2017-08-23 19:49:16 -0700

    set done as well

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage[hash(item)] = item
 };
 
 setPrototype.contains = function(item) {
+  return this.storage[hash(item)] != undefined
 };
 
 setPrototype.remove = function(item) {
+  this.storage[hash(item)] = undefined
 };
+
+let hash = function(item) {
+  let hashValue = ''
+  hashValue = item.toString()
+  return hashValue
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
