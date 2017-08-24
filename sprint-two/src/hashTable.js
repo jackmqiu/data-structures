a73e6429411b270e04a1b32693cd6200cd1b683b
Hack Reactor Student <student@hackreactor.com>
2017-08-24 11:02:04 -0700

    hash Table worked through standard tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,27 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.map = {};
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if(index === 0){
+    this._limit = this._limit * 2;
+    index = getIndexBelowMaxForKey(k, this._limit);
+  }
+  this.map[index] = this.map[index] || {};
+  this.map[index][k] = v;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this.map[index][k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this.map[index][k] = undefined;
 };
 
 
@@ -22,5 +31,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
