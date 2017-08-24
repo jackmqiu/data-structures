92479ce196a032cc6f5139c0692ca9498d4c6ddd
Hack Reactor Student <student@hackreactor.com>
2017-08-24 14:22:39 -0700

    advanced hashTable tests passed

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,26 +4,33 @@ var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
   this.map = {};
+  this.count = 0;
+  this.limitMap = {}
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if(index === 0){
-    this._limit = this._limit * 2;
-    index = getIndexBelowMaxForKey(k, this._limit);
-  }
+  this.count++;
   this.map[index] = this.map[index] || {};
-  this.map[index][k] = v;
+  this.map[index][k] = v
+  this.limitMap[k] = this._limit
+  if(this.count / this._limit >= 0.75){
+    this._limit *= 2;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this.limitMap[k]);
   return this.map[index][k];
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this.limitMap[k]);
+  this.count--;
   this.map[index][k] = undefined;
+  if(this.count / this._limit <= 0.25 && this._limit > 8){
+    this._limit /= 2;
+  }
 };
 
 
