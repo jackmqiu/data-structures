8585949531615b1f83c5768353b6b2f73bcc2e96
Hack Reactor Student <student@hackreactor.com>
2017-08-24 10:12:34 -0700

    tree passes all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,43 @@
 var Tree = function(value) {
   var newTree = {};
+
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  extend(newTree, treeMethods);// your code here
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
+}
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var searchTree = function(tree){
+    if(tree.value === target){
+      return true;
+    }
+    if(tree.children.length != 0){
+      for(var i = 0; i < tree.children.length; i++){
+        if(searchTree(tree.children[i])){
+          return true;
+        }
+
+      }
+    }
+    return false;
+
+  }
+  return searchTree(this);
 };
 
 
